774f437bc394ef36a9557bfd3765edb4f50f0a56
toniodin <74736378+toniodin@users.noreply.github.com>
2023-02-17 18:44:48 +0100

    rama creada

---
+++ LaravelBack/app/Http/Controllers/Ranking.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Ranking extends Controller{
+
+    public function createRanking(Request $request){
+        $request->validate([
+            'nombre' => '',
+            'codigo_sala' => 'required',
+        ]);
+
+       $ranking = new Ranking();
+       $ranking->nombre = $request->nombre;
+       $ranking->codigo_sala = $request->codigo_sala;
+       $ranking->save();
+
+        return response()->json([
+            "status" => 1,
+            "msg" => "¡Registro de usuario al ranking exitoso!",
+        ]);
+    }
+
+
+    public function addToRanking(Request $request){
+        $request->validate([
+            'idUser' => 'required',
+            'codRanking' => 'required',
+        ]);
+
+       $ranking = new Ranking();
+       $ranking->idUser = $request->idUser;
+       $ranking->codRanking = $request->codRanking;
+       $ranking->save();
+
+        return response()->json([
+            "status" => 1,
+            "msg" => "¡Registro de usuario exitoso!",
+        ]);
+    }
+}
