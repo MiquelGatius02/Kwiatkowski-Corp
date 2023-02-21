137b0137581efe950623f3e8204905cc3b7a84b3
Xarus <cezarygebczyk117@gmail.com>
2023-02-21 18:49:14 +0100

    Cosas

--- LaravelBack/app/Http/Controllers/Ranking.php
+++ LaravelBack/app/Http/Controllers/Ranking.php
@@ -6,18 +6,24 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Ranking;
 
+<<<<<<< Updated upstream
 class RankingController extends Controller{
+=======
+class Ranking extends Controller
+{
+>>>>>>> Stashed changes
 
-    public function createRanking(Request $request){
+    public function createRanking(Request $request)
+    {
         $request->validate([
             'nombre' => '',
             'codigo_sala' => 'required',
         ]);
 
-       $ranking = new Ranking();
-       $ranking->nombre = $request->nombre;
-       $ranking->codigo_sala = $request->codigo_sala;
-       $ranking->save();
+        $ranking = new Ranking();
+        $ranking->nombre = $request->nombre;
+        $ranking->codigo_sala = $request->codigo_sala;
+        $ranking->save();
 
         return response()->json([
             "status" => 1,
@@ -26,16 +32,17 @@ class RankingController extends Controller{
     }
 
 
-    public function addToRanking(Request $request){
+    public function addToRanking(Request $request)
+    {
         $request->validate([
             'idUser' => 'required',
             'codRanking' => 'required',
         ]);
 
-       $ranking = new Ranking();
-       $ranking->idUser = $request->idUser;
-       $ranking->codRanking = $request->codRanking;
-       $ranking->save();
+        $ranking = new Ranking();
+        $ranking->idUser = $request->idUser;
+        $ranking->codRanking = $request->codRanking;
+        $ranking->save();
 
         return response()->json([
             "status" => 1,
