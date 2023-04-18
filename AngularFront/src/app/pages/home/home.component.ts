215d0c22fda308f3031b04099b310fa40ad56f18
toniodin <74736378+toniodin@users.noreply.github.com>
2023-04-18 19:27:12 +0200

    Xarus maricon

--- AngularFront/src/app/pages/home/home.component.ts
+++ AngularFront/src/app/pages/home/home.component.ts
@@ -37,13 +37,6 @@ export class HomeComponent implements OnInit {
   showTable: boolean = false;
 
   //Mostrar alertas
-  showAlert: boolean = false;
-  showAlertError: boolean = false;
-  showAlertAceptada: boolean = false;
-  showAlertErrorAceptada: boolean = false;
-  showAlertRankDelete: boolean = false;
-  showAlertRankCodeUpdated: boolean = false
-  showAlertPeticion: boolean = false;
   suscrito: boolean = false;
 
   constructor(
@@ -57,7 +50,7 @@ export class HomeComponent implements OnInit {
 
     this.joinForm = this.fb.group({
 
-      rank_id: '',
+      rank_id:! '',
 
     });
 
@@ -81,7 +74,6 @@ export class HomeComponent implements OnInit {
   }
 
   checkRanking() {
-    console.log(this.rankingService._data1.length)
     if (this.authService.UserData.date != undefined) {
       for (let i = 0; i < this.UserRankingData.length; i++) {
         if (this.UserRankingData[i].user_id == this.profileData.id) {
@@ -117,11 +109,49 @@ export class HomeComponent implements OnInit {
   }
 
   onSubmit() {
-
-    this.joinData = this.joinForm.value;
-    this.joinData.user_logged = this.authService.UserData.id;
-    this.rankingService.addRanking(this.joinData);
-    this.joinForm.reset();
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
+    swalWithBootstrapButtons.fire({
+      title: '¿Desea unirse a este ranking?',
+      text: "!De enviará una petición al administrador de este ranking!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, enviar',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed && this.joinForm.value.rank_id != true) {
+        swalWithBootstrapButtons.fire(
+          'Enviado!',
+          'Podrá acceder al ranking cuando el administrador acepte su peticion.',
+          'success'
+        ).then((result2) => {
+          if (result2.isConfirmed) {
+            this.joinData = this.joinForm.value;
+            this.joinData.user_logged = this.authService.UserData.id;
+            this.rankingService.addRanking(this.joinData);
+            this.joinForm.reset();
+          }
+        })
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'No se ha enviado ninguna solicitud de union.',
+          'error'
+        )
+      } else{
+        swalWithBootstrapButtons.fire(
+          'Cancelado',
+          'No se ha enviado ninguna solicitud de union ya que el input esta vacío.',
+          'error'
+        )
+      }
+    })
   }
 
   createRanking() {
@@ -131,8 +161,25 @@ export class HomeComponent implements OnInit {
     this.crearData.id_creador = this.authService.UserData.id;
     this.rankingService.createRaking(this.crearData).subscribe(
       (result) => {
-        // console.log(result);
-        window.location.reload();
+        const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+          },
+          buttonsStyling: false
+        })
+        swalWithBootstrapButtons.fire({
+          title: 'Se ha creado un ranking',
+          text: "Un ranking ha sido creado!",
+          icon: 'success',
+          confirmButtonText: '¡OK!',
+          reverseButtons: true
+        }).then((result) => {
+          if (result.isConfirmed) {
+            window.location.reload();
+          }else{
+            window.location.reload();
+          }
+        })
       },
       () => {
         this.joinForm.reset();
@@ -143,12 +190,9 @@ export class HomeComponent implements OnInit {
 
   eliminarRanking(rank: RankData) {
     this.rankingService.deleteRanking(rank);
-    this.showAlertRankDelete = true;
     setTimeout(() => {
-      this.showAlertRankDelete = false;
       window.location.reload();
     }, 1000);
-    this.showAlertRankDelete = true;
   }
 
   generateRankCode(): number {
@@ -167,109 +211,90 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  aceptarPeticion() {
-    this.petitionsService.aceptarPeticion(this.PetitionsData[0].id, this.PetitionsData[0].rank_code, this.PetitionsData[0].user_id);
-
-    if (this.petitionsService.Petitions.msg == 'Tenemos estas peticiones') {
-      this.showAlertAceptada = true;
-      setTimeout(() => {
-        this.showAlertAceptada = false;
-        this.verPeticiones();
-      }, 2000);
-      this.showAlertAceptada = true;
-    } else {
-      this.showAlertErrorAceptada = true;
-      setTimeout(() => {
-        this.showAlertErrorAceptada = false;
-        this.verPeticiones();
-      }, 2000);
-      this.showAlertErrorAceptada = true;
-    }
-  }
-
-  denegarPeticion() {
-    this.petitionsService.denegarPeticion(this.PetitionsData[0].id);
-
-    if (this.petitionsService.Petitions.msg == 'Tenemos estas peticiones') {
-      this.showAlert = true;
-      setTimeout(() => {
-        this.showAlert = false;
-        this.verPeticiones();
-      }, 2000);
-      this.showTable = true;
-    } else {
-      this.showAlertError = true;
-      setTimeout(() => {
-        this.showAlertError = false;
-        this.verPeticiones();
-      }, 2000);
-      this.showAlertError = true;
-    }
-  }
-
   regenerarCodigo(rank: RankData) {
     let codeNuevo = this.generateRankCode();
     this.rankingService.regenerarCodigo(rank, codeNuevo);
     setTimeout(() => {
       window.location.reload();
-    }, 100);
+    }, 500);
   }
 
-  public modalTitle: string = '';
-  actualizarRank: any = { id: 0, rank_name: "", rank_description: "", id_creador: 0 };
-
-
   public setModalTitle(data: string, rank:RankData): void {
-    this.modalTitle = data;
+    
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false
+    })
 
-    if(this.modalTitle == 'Regenerar'){
-      this.modalTitle = 'Se ha generado un código nuevo de ranking';
-      this.actualizarRank.id = rank.id;  
-      this.actualizarRank.rank_name = rank.rank_name;
-      this.actualizarRank.rank_description = rank.rank_description;
-      this.actualizarRank.id_creador = rank.id_creador; 
-
-      const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-          confirmButton: 'btn btn-success',
-          cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-      })
+    if(data == 'Regenerar'){
       
       swalWithBootstrapButtons.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        title: '¿Regenerar el código?',
+        text: "!El código del ranking cambiará!",
         icon: 'warning',
         showCancelButton: true,
-        confirmButtonText: 'Yes, delete it!',
-        cancelButtonText: 'No, cancel!',
+        confirmButtonText: 'Sí, cambiarlo',
+        cancelButtonText: 'No, cancelar!',
         reverseButtons: true
       }).then((result) => {
         if (result.isConfirmed) {
           swalWithBootstrapButtons.fire(
-            'Deleted!',
-            'Your file has been deleted.',
+            'Cambiado!',
+            'Su código de ranking ha cambiado.',
             'success'
-          )
+          ).then((result2) => {
+            if (result2.isConfirmed) {
+              this.regenerarCodigo(rank);
+            }
+          })
         } else if (
           /* Read more about handling dismissals below */
           result.dismiss === Swal.DismissReason.cancel
         ) {
           swalWithBootstrapButtons.fire(
-            'Cancelled',
-            'Your imaginary file is safe :)',
+            'Cancelado',
+            'No se ha regenerado el código de ranking',
             'error'
           )
         }
       })
       
-    }else if(this.modalTitle == 'Cooperación'){
-     
-    }else if(this.modalTitle == 'Habilidades de pensamiento'){
-     
-    }else if(this.modalTitle == 'Gestión emocional'){
+    }else if(data == 'Eliminar'){
+      
+      swalWithBootstrapButtons.fire({
+        title: '¿Eliminar el ranking?',
+        text: "!El ranking y los alumnos asociados a este serán eliminados!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, eliminarlo',
+        cancelButtonText: 'No, cancelar!',
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire(
+            'Eliminado!',
+            'Se ha eliminado un ranking.',
+            'success'
+          ).then((result2) => {
+            if (result2.isConfirmed) {
+              this.eliminarRanking(rank);
+            }
+          })
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Cancelado',
+            'No se ha eliminado ningún ranking',
+            'error'
+          )
+        }
+      })
+    }
   }
-}
 }
 
